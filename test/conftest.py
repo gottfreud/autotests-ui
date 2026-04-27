50700b255e468e12e8346177b2427537fa1c010c
gottfreud <github@sandalow.net>
2026-04-27 13:25:16 +0200

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- test/conftest.py
+++ test/conftest.py
@@ -8,3 +8,24 @@ def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
+
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright):
+    chromium_page = playwright.chromium.launch(headless=False).new_page()
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill("username")
+    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill("password")
+    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+    chromium_page.context.storage_state(path="browser-state.json")
+
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    yield context.new_page()
+    browser.close()
