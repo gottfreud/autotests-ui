3a3d59aec09bdb7dcaf81fa80e4ad5066e9ffdef
gottfreud <github@sandalow.net>
2026-05-05 11:25:37 +0200

    Практикуемся в реализации PageObject

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -7,23 +7,19 @@ class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Заголовок и кнопка создания курса
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
 
-        # Карточка курса
         self.course_title = page.get_by_test_id('course-widget-title-text')
         self.course_image = page.get_by_test_id('course-preview-image')
         self.course_max_score_text = page.get_by_test_id('course-max-score-info-row-view-text')
         self.course_min_score_text = page.get_by_test_id('course-min-score-info-row-view-text')
         self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
 
-        # Меню курса
         self.course_menu_button = page.get_by_test_id('course-view-menu-button')
         self.course_edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
         self.course_delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
 
-        # Пустой блок при отсутствии курсов
         self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
         self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
         self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
@@ -51,15 +47,14 @@ class CoursesListPage(BasePage):
 
     def check_visible_course_card(
             self,
-            index: int,  # Индекс карточки в списке курсов
-            title: str,  # Ожидаемый заголовок курса
-            max_score: str,  # Ожидаемый максимальный балл
-            min_score: str,  # Ожидаемый минимальный балл
-            estimated_time: str  # Ожидаемое время прохождения
+            index: int,
+            title: str,
+            max_score: str,
+            min_score: str,
+            estimated_time: str
     ):
         expect(self.course_image.nth(index)).to_be_visible()
 
-        # Обратите внимание на использование метода nth, он позволяет получить локатор по индексу
         expect(self.course_title.nth(index)).to_be_visible()
         expect(self.course_title.nth(index)).to_have_text(title)
 
@@ -77,7 +72,6 @@ class CoursesListPage(BasePage):
     def click_edit_course(self, index: int):
         self.course_menu_button.nth(index).click()
 
-        # После нажатия на кнопку меню обязательно проверяем, что меню открылась и меню айтем виден
         expect(self.course_edit_menu_item.nth(index)).to_be_visible()
         self.course_edit_menu_item.nth(index).click()
 
--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -7,17 +7,14 @@ class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        # Заголовок и кнопка создания курса
         self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
 
-        # Картинка предпросмотра и блок предпросмотра картинки курса
         self.preview_image = page.get_by_test_id('create-course-preview-image-upload-widget-preview-image')
         self.preview_empty_view_icon = page.get_by_test_id('create-course-preview-empty-view-icon')
         self.preview_empty_view_title = page.get_by_test_id('create-course-preview-empty-view-title-text')
         self.preview_empty_view_description = page.get_by_test_id('create-course-preview-empty-view-description-text')
 
-        # Кнопка загрузки, удаления картинки предпросмотра курса и блок с информацией о загружаемой картинке
         self.preview_image_upload_icon = page.get_by_test_id('create-course-preview-image-upload-widget-info-icon')
         self.preview_image_upload_title = page.get_by_test_id(
             'create-course-preview-image-upload-widget-info-title-text'
@@ -33,23 +30,19 @@ class CreateCoursePage(BasePage):
         )
         self.preview_image_upload_input = page.get_by_test_id('create-course-preview-image-upload-widget-input')
 
-        # Форма создания курса
         self.create_course_title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
         self.create_course_estimated_time_input = (
             page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
         )
         self.create_course_description_textarea = (
-            # При поиске поля описания будет найдено два тега textarea, берем первый из них
             page.get_by_test_id('create-course-form-description-input').locator('textarea').first
         )
         self.create_course_max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
         self.create_course_min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
 
-        # Заголовок и кнопка создания задания
         self.exercises_title = page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
         self.create_exercise_button = page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
 
-        # Блок, который отображется, когда в курсе нет заданий
         self.exercises_empty_view_icon = page.get_by_test_id('create-course-exercises-empty-view-icon')
         self.exercises_empty_view_title = page.get_by_test_id('create-course-exercises-empty-view-title-text')
         self.exercises_empty_view_description = page.get_by_test_id(
@@ -173,12 +166,6 @@ class CreateCoursePage(BasePage):
         )
 
     def click_delete_exercise_button(self, index: int):
-        # Обратите внимание, что локатор инициализируется непосредственно в методе.
-        # Это временное решение, так как с классическим подходом POM сложно работать с динамическими локаторами.
-        # В текущей реализации мы не можем заранее объявить локатор на уровне класса, поскольку его значение
-        # зависит от переданного индекса.
-        # В дальнейшем мы будем использовать паттерн PageFactory для более удобной обработки таких случаев
-        # и динамических элементов на странице.
         delete_exercise_button = self.page.get_by_test_id(
             f"create-course-exercise-{index}-box-toolbar-delete-exercise-button"
         )
