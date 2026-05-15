83b063a237b156513732f3840eeec3e33acb6a7f
gottfreud <github@sandalow.net>
2026-05-15 11:36:09 +0200

    Наращивание тестовой базы и рефакторинг

---
+++ test/dashboard/test_dashboard.py
@@ -0,0 +1,15 @@
+from pages.dashboard.dashboard_page import DashboardPage
+import pytest
+
+@pytest.mark.regression
+@pytest.mark.dashboard
+class TestDashboard:
+    def test_dashboard_displaying(self, dashboard_page_with_state: DashboardPage):
+        dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        dashboard_page_with_state.sidebar.check_visible()
+        dashboard_page_with_state.navbar.check_visible("username")
+        dashboard_page_with_state.toolbar.check_visible()
+        dashboard_page_with_state.check_visible_students_chart()
+        dashboard_page_with_state.check_visible_courses_chart()
+        dashboard_page_with_state.check_visible_scores_chart()
+        dashboard_page_with_state.check_visible_activities_chart()
