cd6ef1a3de2ef8cc48fe09112899674f56d0fd40
gottfreud <github@sandalow.net>
2026-05-15 11:50:46 +0200

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- test/courses/test_courses.py
+++ test/courses/test_courses.py
@@ -31,3 +31,15 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.course_view.check_visible(index=0, estimated_time="2 weeks", title="Playwright",
                                                     max_score="100", min_score="10")
+
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.create_course_form.fill(title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10")
+        create_course_page.image_upload_widget.upload_preview_image("testdata/files/image.png")
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(index=0, estimated_time="2 weeks", title="Playwright", max_score="100", min_score="10")
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.fill(title="Playwright Edited", estimated_time="3 weeks", description="Playwright Edited", max_score="200", min_score="20")
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+        courses_list_page.course_view.check_visible(index=0, estimated_time="3 weeks", title="Playwright Edited", max_score="200", min_score="20")
