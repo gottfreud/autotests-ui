73d65c5b0cee6e6db7450ae383d57715af05e287
gottfreud <github@sandalow.net>
2026-05-16 08:17:19 +0200

    Настройки автотестов с Pydantic. Практика работы с настройками

--- test/authentication/test_registration.py
+++ test/authentication/test_registration.py
@@ -1,5 +1,7 @@
 import pytest
 import allure
+
+from config import settings
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registration_page import RegistrationPage
 from tools.allure.epics import AllureEpic
@@ -7,6 +9,8 @@ from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
 from allure_commons.types import Severity
+
+from tools.routes import AppRoute
 
 
 @pytest.mark.regression
@@ -22,8 +26,8 @@ class TestRegistration:
     @allure.title("Registration with correct email, username and password")
     @allure.severity(Severity.CRITICAL)
     def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
-        registration_page.registration_form.check_visible('user.name@gmail.com', 'username', 'password')
+        registration_page.visit(AppRoute.REGISTRATION)
+        registration_page.registration_form.fill(email=settings.test_user.email, username=settings.test_user.username, password=settings.test_user.password)
+        registration_page.registration_form.check_visible(settings.test_user.email, settings.test_user.username, settings.test_user.password)
         registration_page.click_registration_button()
         dashboard_page.toolbar.check_visible()
--- test/dashboard/test_dashboard.py
+++ test/dashboard/test_dashboard.py
@@ -1,3 +1,4 @@
+from config import settings
 from pages.dashboard.dashboard_page import DashboardPage
 import allure
 import pytest
@@ -6,6 +7,7 @@ from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
 from tools.allure.stories import AllureStory
 from tools.allure.tags import AllureTag
+from tools.routes import AppRoute
 
 
 @pytest.mark.regression
@@ -21,9 +23,9 @@ class TestDashboard:
     @allure.title("Check displaying of dashboard page")
     @allure.severity(Severity.NORMAL)
     def test_dashboard_displaying(self, dashboard_page_with_state: DashboardPage):
-        dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        dashboard_page_with_state.visit(AppRoute.DASHBOARD)
         dashboard_page_with_state.sidebar.check_visible()
-        dashboard_page_with_state.navbar.check_visible("username")
+        dashboard_page_with_state.navbar.check_visible(settings.test_user.username)
         dashboard_page_with_state.toolbar.check_visible()
         dashboard_page_with_state.check_visible_students_chart()
         dashboard_page_with_state.check_visible_courses_chart()
