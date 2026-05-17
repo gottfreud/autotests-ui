85ea2061ea62e92dbb862791301b8a2e795721b4
gottfreud <github@sandalow.net>
2026-05-17 10:24:49 +0200

    Удаление артефактов покрытия

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class TestData(BaseModel):
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', env_nested_delimiter='.')
+    model_config = SettingsConfigDict(extra='allow', env_file='.env', env_file_encoding='utf-8', env_nested_delimiter='.')
     app_url: HttpUrl
     headless: bool
     browsers: list[Browser]
--- elements/input.py
+++ elements/input.py
@@ -3,7 +3,7 @@ import allure
 from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
 from tools.logger import get_logger
-
+from ui_coverage_tool import ActionType
 logger = get_logger("INPUT")
 
 class Input(BaseElement):
@@ -14,6 +14,13 @@ class Input(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Переопределяем метод формирования XPath-селектора:
+        #  - сначала получаем общий селектор блока
+        #  - затем уточняем путь до самого <input>, добавляя '//input'
+        # Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие.
+        return f'{super().get_raw_locator(**kwargs)}//input'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -21,9 +28,15 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             locator.fill(value)
 
+        # После успешного fill фиксируем покрытие как действие FILL
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
         with allure.step(step):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_have_value(value)
+
+        # Фиксируем в покрытии, что значение проверено — тип VALUE
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
